a12efd465f6fff1afc0d799dae60f77ed206ef82
Ahmad Moussa <amm65@mail.aub.edu>
2019-04-17 16:50:35 +0300

    Create wrapper classes for the openCV frame for additional functionality

--- vidtomu.py
+++ vidtomu.py
@@ -3,10 +3,92 @@ import cv2
 
 cap = cv2.VideoCapture(0)
 
-# def subdivide(frame, numOfSubFrms):
-#    imgCells = []
-#    xRange = (frame.shape[0]/numOfSubFrms)
-#    for x in range(xRange)
+#   I think I will need to create a custom class for my purposes
+#   as well as a grid listener
+
+#   Grid Frame Class that will serve as base for my project
+#   It'll carry the frame and the Grid Listener
+class GridFrame:
+    def __init__(self, frame, cellSize):
+        self.frame = frame
+        self.cellSize = cellSize
+        self.fHeight, self.fWidth, self.fChannels = frame.shape
+
+        #   Array that later will hold the cells of the grid
+        self.GridCells = []
+        self.createGrid()
+
+    #   Shows the frame that is held by the GridFrame
+    def showGridFrame(self):
+        cv2.imshow('frame',self.frame)
+
+    #   Attempts to show A cell of the grid
+    def showCell(self, cellNumber):
+        self.GridCells[cellNumber].showC()
+
+    #   Show all cells
+    def showCells(self):
+        for c in self.GridCells:
+            c.showC()
+
+    #   Also populates the GridCells Array that is a member of this class
+    #   According to the cellSize specified on init
+    def createGrid(self):
+        cellHeight = int(self.fHeight/self.cellSize)
+        cellWidth = int(self.fWidth/self.cellSize)
+        for x in range(0,self.cellSize):
+            for y in range(0, self.cellSize):
+                # Create a new GridCell Instance and initialize
+                cell = GridCell(cellHeight, cellWidth, cellHeight*y, cellWidth*x,
+                                self.frame[cellHeight*y:cellHeight*y+cellHeight,cellWidth*x:cellWidth*x+cellWidth])
+                self.GridCells.append(cell)
+
+# Meta Object that will describe a singular cell in the GridFrame
+# Will be used to find events occuring inside these cells
+class GridCell():
+    def __init__(self, height, width, startX, startY, partialFrame):
+        self.height = height
+        self.width = width
+        self.startX = startX
+        self.startY = startY
+        self.partialFrame = partialFrame
+
+    def showC(self):
+        cv2.imshow('cell',self.partialFrame)
+
+    # should create an overlay with an translucent color
+    def drawBorders(self):
+        # Color the border of each cell here
+        x = 0
+
+
+#Divides Image or frame into several images depending on subdivision rate
+def subdivide(frame, numOfSubFrms):
+    #cloning the original frame such that we'll be working on a copy from now on
+    frameC = np.array(frame, copy = True)
+
+    # storing frame information such that we can do operations on it
+    frameHeight, frameWidth, channels = frameC.shape
+    #instantiate empty array that will hold the image cells later on
+    imgCells = []
+
+    # determining the cell size given the desired number of sub divisions
+    cellHeight = int(frameHeight/numOfSubFrms)
+    cellWidth = int(frameWidth/numOfSubFrms)
+
+    cells = []
+    # extracting cells from the frame copy
+    for x in range(0,numOfSubFrms):
+        for y in range(0, numOfSubFrms):
+            imgCells.append(frameC[cellHeight*y:cellHeight*y+cellHeight,cellWidth*x:cellWidth*x+cellWidth])
+
+    print(len(imgCells))
+    for c in imgCells:
+        cv2.imshow('cell',c)
+
+    return imgCells
+
+
 
 def colorMask(frame, lower, upper):
     # converting RGB to HSV
@@ -44,14 +126,28 @@ while(1):
     # as I understand it, historically, the underscore is used to indicate that part of the result of a function is being deliberately ignored
     _, frame = cap.read()
 
-    lower_bound = np.array([110,50,50])
-    upper_bound = np.array([255, 173, 96])
+    #https://dsp.stackexchange.com/questions/2687/why-do-we-use-the-hsv-colour-space-so-often-in-vision-and-image-processing
+    # I should look into using a hsv colorspace instead, it seems more useful than rgb in this specific case
+    colorDict = {
+        'blue':[[86, 31, 4], [220, 88, 50]],
+        'red':[[17, 15, 100], [50, 56, 200]],
+        'yellow':[[25, 146, 190], [62, 174, 250]],
+        'gray':[[103, 86, 65], [205, 133, 188]]
+        }
+    lower_bound = np.array(colorDict['gray'][0])
+    upper_bound = np.array(colorDict['gray'][1])
 
     image, mask, res = colorMask(frame, lower_bound, upper_bound)
-    show(image, mask, res)
+    #show(image, mask, res)
+
+    #subdivide(frame, 2)
+
+    GF = GridFrame(frame,2)
+    GF.showGridFrame()
+    GF.showCells()
 
     k = cv2.waitKey(5) & 0xFF
     if k == 27:
-     break
+        break
 
 cv2.destroyAllWindows()
